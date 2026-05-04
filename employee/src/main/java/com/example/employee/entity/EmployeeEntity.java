b6345467c73bb7c600fe507d15e7f643a07fa0c1
M.Taha <tahasajjad678@gmail.com>
2026-05-04 12:36:00 +0500

    updated

---
+++ employee/src/main/java/com/example/employee/entity/EmployeeEntity.java
@@ -0,0 +1,59 @@
+package com.example.employee.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private String userType;
+    private String deptName;
+
+    // ===== GETTERS & SETTERS =====
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    } 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
