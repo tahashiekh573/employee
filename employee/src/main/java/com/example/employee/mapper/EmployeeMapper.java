836eeea19eeba9f60809bd639dc4ef969ee7e44f
M.Taha <tahasajjad678@gmail.com>
2026-05-07 15:05:15 +0500

    add new files and new code

--- employee/src/main/java/com/example/employee/mapper/EmployeeMapper.java
+++ employee/src/main/java/com/example/employee/mapper/EmployeeMapper.java
@@ -1,5 +1,39 @@
 package com.example.employee.mapper;
 
+import com.example.employee.dto.EmployeeDto;
+import com.example.employee.entity.EmployeeEntity;
+
 public class EmployeeMapper {
-    
+
+    // ENTITY -> DTO
+    public static EmployeeDto toDto(EmployeeEntity entity) {
+
+        EmployeeDto dto = new EmployeeDto();
+
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        dto.setEmail(entity.getEmail());
+        dto.setUserType(entity.getUserType());
+
+        if (entity.getDepartment() != null) {
+
+            dto.setDeptId(entity.getDepartment().getId());
+            dto.setDeptName(entity.getDepartment().getDeptName());
+        }
+
+        return dto;
+    }
+
+    // DTO -> ENTITY
+    public static EmployeeEntity toEntity(EmployeeDto dto) {
+
+        EmployeeEntity entity = new EmployeeEntity();
+
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+        entity.setEmail(dto.getEmail());
+        entity.setUserType(dto.getUserType());
+
+        return entity;
+    }
 }
