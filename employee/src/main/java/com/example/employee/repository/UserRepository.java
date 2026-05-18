4dd741b319b269ae4bf561baad1cdf6cb187800a
M.Taha <tahasajjad678@gmail.com>
2026-05-18 12:36:01 +0500

    Delete employee/src/main/java/com/example/employee/repository/UserRepository.java

--- employee/src/main/java/com/example/employee/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.employee.entity.UserEntity;
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-
-    UserEntity findByEmail(String email);
-}
